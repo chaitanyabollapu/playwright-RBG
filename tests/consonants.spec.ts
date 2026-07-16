33805a38e74962f0eb2081f3391a9c11b3028c35
chaitanyabollapu <chaitanyabollapu1007@gmail.com>
2026-07-16 21:30:18 +0530

    modified file

--- tests/consonants.spec.ts
+++ tests/consonants.spec.ts
@@ -1,13 +1,16 @@
 import { test} from '@playwright/test';
 test('vowel and consonant count',async()=>{
-let str="Automation Testing";
+let str="Automation testing";
 let vowels =0;
 let consonants=0;
 str=str.toLowerCase();
-for (let i=0;i<=str.length;i++){
-    if(str[i]>='a'&&str[i]<='z'){
+for (let i=0;i<str.length;i++){
+    if(str[i]>='a'&& str[i]<='z'){
 
     }
+     if (str[i] === ' ') {
+        continue;
+     }
     if(
         str[i]==='a' ||
         str[i]==='e' ||
