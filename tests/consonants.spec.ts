43e0ffe549c9860eb8a757512c6e71be32ea31f0
chaitanyabollapu <chaitanyabollapu1007@gmail.com>
2026-07-16 21:18:33 +0530

    vowels&consonants

---
+++ tests/consonants.spec.ts
@@ -0,0 +1,26 @@
+import { test} from '@playwright/test';
+test('vowel and consonant count',async()=>{
+let str="Automation Testing";
+let vowels =0;
+let consonants=0;
+str=str.toLowerCase();
+for (let i=0;i<=str.length;i++){
+    if(str[i]>='a'&&str[i]<='z'){
+
+    }
+    if(
+        str[i]==='a' ||
+        str[i]==='e' ||
+        str[i]==='i' ||
+        str[i]==='o' ||
+        str[i]==='u' 
+    ){
+        vowels++
+    }
+    else{
+        consonants++
+    }
+}
+console.log("vowels count:",vowels)
+console.log("consonants count:",consonants)
+});
